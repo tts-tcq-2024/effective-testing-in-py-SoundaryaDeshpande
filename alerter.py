904d7c2fe45b74158bc593ee0b498bc5a629a03f
SoundaryaDeshpande <98644073+SoundaryaDeshpande@users.noreply.github.com>
2024-08-09 10:17:45 +0530

    SoundaryaD_Alter4

--- alerter.py
+++ alerter.py
@@ -23,5 +23,6 @@ def alert_in_celcius(farenheit):
 
 alert_in_celcius(400.5)
 alert_in_celcius(303.6)
+alert_in_celcius(50)
 print(f'{alert_failure_count} alerts failed.')
 print('All is well (maybe!)')
