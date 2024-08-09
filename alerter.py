e4c40722284f08db7e7157076ad454a728f84642
SoundaryaDeshpande <98644073+SoundaryaDeshpande@users.noreply.github.com>
2024-08-09 09:23:34 +0530

    SoundaryaD_alerter1

--- alerter.py
+++ alerter.py
@@ -1,15 +1,17 @@
 alert_failure_count = 0
 
-def network_alert_stub(celcius):
+def network_alert_stub(celcius,simluate_failure=False):
     print(f'ALERT: Temperature is {celcius} celcius')
     # Return 200 for ok
     # Return 500 for not-ok
     # stub always succeeds and returns 200
-    return 200
-
+    return 500 if simulate_failure else 200
+    
 def alert_in_celcius(farenheit):
     celcius = (farenheit - 32) * 5 / 9
-    returnCode = network_alert_stub(celcius)
+    simulate_failure=celcius>100
+    returnCode = network_alert_stub(celcius,simulate_failure)
+    
     if returnCode != 200:
         # non-ok response is not an error! Issues happen in life!
         # let us keep a count of failures to report
